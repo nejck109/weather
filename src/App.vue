d468842ed1782c25dda96c941907bb719f8e26e7
nejck109 <78321993+nejck109@users.noreply.github.com>
2021-10-14 12:01:24 +0200

    Added config file
    Added config file, so that I can publish this project on subfolder on my domain.

--- src/App.vue
+++ src/App.vue
@@ -34,40 +34,47 @@ export default {
     }
   },
   methods: {
-     async fetchWeather(value) {
+
+    alreadyInList(){
+      if(this.searchHistoryArray.includes(this.searchString)){
+          var ind = this.searchHistoryArray.indexOf(this.searchString);  // If it is, remember index
+          this.searchHistoryArray.splice(ind, 1); // Delete it
+        }
+    },
+    maxFiveItems(){
+       if(this.searchHistoryArray.length > 5){ 
+            this.searchHistoryArray.pop(); // If we already have 5 history items, delete the last one.
+        }
+    },
+    async fetchWeather(value) {
         
-        this.searchString = value.toLowerCase(); // get search string and transform to lower case - (includes method case sensitive)
+      this.searchString = value.toLowerCase(); // get search string and transform to lower case - (includes method case sensitive)
         
-        // Fetch data
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.searchString}&appid=8d071f92b0322fc48b1601f4f2e567ce`)
+      // Fetch data
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.searchString}&appid=8d071f92b0322fc48b1601f4f2e567ce`)
             
 
-            // Check if response was valid 
-            if(!res.ok){
+      // Check if response was valid 
+      if(!res.ok){
 
-                this.cityNotValid = true; // Set city boolean to true if city was not valid
-                this.showWeatherBlock = false; // Do not show weather for invalid response
-            }else{
-                this.cityNotValid = false;
+        this.cityNotValid = true; // Set city boolean to true if city was not valid
+        this.showWeatherBlock = false; // Do not show weather for invalid response
+      }else{
+        this.cityNotValid = false;
 
-                // Check if search string is already in history list
-                if(this.searchHistoryArray.includes(this.searchString)){
-                    var ind = this.searchHistoryArray.indexOf(this.searchString);  // If it is, remember index
-                    this.searchHistoryArray.splice(ind, 1); // Delete it
-                }
+        // Check if search string is already in history list
+        this.alreadyInList();
 
-                this.searchHistoryArray.unshift(this.searchString); // Add at the beggining of search history
-
-                if(this.searchHistoryArray.length > 5){ 
-                    this.searchHistoryArray.pop(); // If we already have 5 history items, delete the last one.
-                }
+        this.searchHistoryArray.unshift(this.searchString); // Add at the beggining of search history
                 
-                const data = await res.json(); // Get data
-                this.returnedData = data; // Save data
-                this.showWeatherBlock = true; // Show weather for responce
-                return data;
-              }
-     },
+        this.maxFiveItems();
+
+        const data = await res.json(); // Get data
+        this.returnedData = data; // Save data
+        this.showWeatherBlock = true; // Show weather for responce
+        return data;
+      }
+    },
 
   }
 }
